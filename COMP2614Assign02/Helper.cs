d764d7766b54af8a0796d5f59aa595d0ea99b3e1
Kris <kris_s2@outlook.com>
2017-04-28 20:55:56 -0700

    Add project files.

---
+++ COMP2614Assign02/Helper.cs
@@ -0,0 +1,22 @@
+﻿namespace COMP2614Assign02
+{
+	public class Helper
+	{
+		public enum Province
+		{
+			Alberta,	// = "AB",
+			BC,			//British Columbia,// = "BC",
+			MB,			//"Manitoba" = "MB",
+			NB,			//"New Brunswick"= "NB",
+			NL,			//"Newfoundland and Labrador"= "NL",
+			NT,			//"Northwest Territories"= "NT",
+			NS,			// "Nova Scotia"= "NS",
+			NU,			//"Nunavut"= "NU",
+			ON,			//"Ontario"= "ON",
+			PE,			//"Prince Edward Island"= "PE",
+			QC,			//"Québec"= "QC",
+			SK,			//"Saskatchewan"= "SK",
+			YT			// "Yukon Territory"= "YT"
+		}
+	}
+}
---
+++ COMP2614Assign02/MyApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP2614Assign02
+{
+	class MyApp
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+
+		public static void Run()
+		{
+			Helper myHelper = new Helper();
+		}
+	}
+}
